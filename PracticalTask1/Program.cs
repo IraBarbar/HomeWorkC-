56fdab3c21ad3714122d468e0869256cd04bc664
IraBarbar <bro998877@yahoo.com>
2022-10-13 14:49:34 +0300

    Task4

--- PracticalTask1/Program.cs
+++ PracticalTask1/Program.cs
@@ -6,6 +6,7 @@ a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
 
+/*
 Console.Write("Enter the first number: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter the second number: ");
@@ -19,3 +20,30 @@ else
 {
     Console.Write($"{number2} - max, {number1} - min ");
 }
+*/
+
+// Задача 4: Напишите программу, которая принимает на вход три числа 
+//и выдаёт максимальное из этих чисел.
+
+/*
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.Write("Enter the first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the third number: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if(number2 > max) max = number2;
+if(number3 > max) max = number3;
+
+Console.WriteLine($" Max is {max} ");
+
+
+
