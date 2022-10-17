a905dcab9598b7afc3c655a3686f6a80172ffd44
IraBarbar <bro998877@yahoo.com>
2022-10-17 20:24:31 +0300

    Practical2_Test13

--- PracticalTask2/Program.cs
+++ PracticalTask2/Program.cs
@@ -7,6 +7,7 @@
 918 -> 1
 */
 
+/*
 bool TestThird(int N)
 {
     if( N > 99 && N < 1000) return true;
@@ -20,7 +21,6 @@ int FindSecond(int N)
     return sec;
 }
 
-
 Console.WriteLine("Please, enter the three-digit number.");
 int n = Convert.ToInt32(Console.ReadLine());
 
@@ -30,4 +30,42 @@ int result = FindSecond(n);
 
 if (test == true) Console.WriteLine(result);
 if (test == false) Console.Write(" The number is not the three-digit number.");
+*/
 
+/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+ или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/ 
+bool TestNumber(int N)
+{
+    if( N > 99) return true;
+    else return false;
+}
+int FindThirdNumber(int N)
+{
+    int ed = N % 10;
+    return ed;
+}
+
+Console.WriteLine("Please, enter the  number.");
+int n = Convert.ToInt32(Console.ReadLine());
+
+bool num = TestNumber(n);
+
+if (num == false) Console.WriteLine("The third number is not .");
+
+int newn = n;
+
+while (n > 1000)
+{
+    newn = n / 10;
+    n = newn;
+}
+n = FindThirdNumber(n);
+Console.WriteLine(n);
