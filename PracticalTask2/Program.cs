d7eaa4b2806928297626654621f6bc2316f40a0a
IraBarbar <bro998877@yahoo.com>
2022-10-18 13:36:43 +0300

    Upgrate Test10

--- PracticalTask2/Program.cs
+++ PracticalTask2/Program.cs
@@ -1,4 +1,44 @@
-﻿/* Задача 10.
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+ или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+
+bool TestNumber(int N)
+{
+    if( N > 99) return true;
+    else return false;
+}
+int FindThirdNumber(int N)
+{
+    int ed = N % 10;
+    return ed;
+}
+
+Console.WriteLine("Please, enter the  number.");
+int n = Convert.ToInt32(Console.ReadLine());
+
+bool num = TestNumber(n);
+
+if (num == false) Console.WriteLine("The third number is not .");
+if (num == true)
+{
+int newn = n;
+
+while (n > 1000)
+{
+    newn = n / 10;
+    n = newn;
+}
+int result = FindThirdNumber(n);
+Console.Write(result);
+}
+/*
+/* Задача 10.
 // Напишите программу, которая принимает на вход трёхзначное число
  и на выходе показывает вторую цифру этого числа.
 
@@ -7,7 +47,7 @@
 918 -> 1
 */
 
-/*
+
 bool TestThird(int N)
 {
     if( N > 99 && N < 1000) return true;
@@ -26,46 +66,18 @@ int n = Convert.ToInt32(Console.ReadLine());
 
 bool test = TestThird(n);
 
+bool newtest = test;
+
+while (test == false)
+{
+    Console.WriteLine(" The number is not the three-digit number. Please, enter the three-digit number.");
+    int n1 = Convert.ToInt32(Console.ReadLine());
+    newtest = TestThird(n1);
+    n = n1;
+    test = newtest;
+    }
+
 int result = FindSecond(n);
 
-if (test == true) Console.WriteLine(result);
-if (test == false) Console.Write(" The number is not the three-digit number.");
-*/
 
-/*
-Задача 13: Напишите программу, которая выводит третью цифру заданного числа
- или сообщает, что третьей цифры нет.
-
-645 -> 5
-
-78 -> третьей цифры нет
-
-32679 -> 6
-*/ 
-bool TestNumber(int N)
-{
-    if( N > 99) return true;
-    else return false;
-}
-int FindThirdNumber(int N)
-{
-    int ed = N % 10;
-    return ed;
-}
-
-Console.WriteLine("Please, enter the  number.");
-int n = Convert.ToInt32(Console.ReadLine());
-
-bool num = TestNumber(n);
-
-if (num == false) Console.WriteLine("The third number is not .");
-
-int newn = n;
-
-while (n > 1000)
-{
-    newn = n / 10;
-    n = newn;
-}
-n = FindThirdNumber(n);
-Console.WriteLine(n);
+Console.WriteLine(result);
