88d917360257eb22e6415f78712f56f19eaf6038
IraBarbar <bro998877@yahoo.com>
2022-11-07 15:48:34 +0300

    Task25

---
+++ PracticalTask4/Program.cs
@@ -0,0 +1,32 @@
+﻿   //№25с Напишите цикл, который принимает на вход числа (А и В) и возводит число А в натуральную степень В.
+   /*
+    int FindDegree(int a , int b)
+    {
+        int product = a;
+        for(int i = 2 ; i <= b ; i++)
+        product = a * product;
+        return product;
+    }
+
+    bool TestNum (int b)
+    {
+        if (b>0) return true;
+        else return false;
+    }      
+
+    Console.Write("Input  number A ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input  natural number B ");
+    int b = Convert.ToInt32(Console.ReadLine());
+
+        if (TestNum (b) == false) 
+        {
+            Console.WriteLine($"Wrong number! Input  natural number B");
+            int b2 = Convert.ToInt32(Console.ReadLine());
+            b = b2;
+        }    
+    
+    Console.WriteLine($"{FindDegree(a,b)}");
+*/
+
+
