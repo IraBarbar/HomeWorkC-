2371f8208d90d6dca262f383aee740cf29e666be
IraBarbar <bro998877@yahoo.com>
2022-11-15 10:20:42 +0300

    SumPositiveElement from User

---
+++ PracticalTask6/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+void ShowArray ( int [] array)
+{
+    for ( int i = 0; i < array.Length; i++)
+        Console.Write(array [i] + " ");
+    Console.WriteLine(); 
+}
+
+int[] FillArray(int size)
+{
+   int [] arr = new int [size];
+   int a = 0;
+   for(int i = 0; i < size; i++)
+   {
+    Console.WriteLine("Input a number: ");
+    a = Convert.ToInt32(Console.ReadLine());
+    
+    int temp = arr[i];
+    arr[i] = a;
+    a = temp;
+   }
+return arr;
+}
+
+int SumPositiveElement(int [] array)
+{
+    int sum = 0;
+    for(int i =0; i < array.Length; i ++)
+    {
+        if (array[i] > 0) sum ++ ;
+    }
+return sum;
+}
+
+Console.WriteLine("How many numbers are you planning to ente?");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [] array = FillArray(n);
+ShowArray (array);
+
+Console.WriteLine("Positive elements are " + SumPositiveElement(array));
+
+
+
+
